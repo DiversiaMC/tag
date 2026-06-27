6e339ba643b2f529fc1d2d9e5b75cf37f7e77aee
TheNolle <github@thenolle.com>
2026-06-27 13:12:02 +0200

    chore: init

---
+++ src/main/kotlin/com/nolly/mc/diversia/tag/DiversiaTag.kt
@@ -0,0 +1,14 @@
+package com.nolly.mc.diversia.tag
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DiversiaTag : JavaPlugin() {
+
+	override fun onEnable() {
+		// Plugin startup logic
+	}
+
+	override fun onDisable() {
+		// Plugin shutdown logic
+	}
+}
